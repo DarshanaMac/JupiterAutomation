35db82764958a296786017c0890d73eb1e7d82b3
Darshana <thaveeshadarshana@gmail.com>
2021-09-12 15:41:56 +0530

    comment test data line
    comment test data line

--- src/test/java/com/jupiter/automation/testsuites/Test4.java
+++ src/test/java/com/jupiter/automation/testsuites/Test4.java
@@ -35,8 +35,8 @@ public class Test4  extends Initializer {
     public Object[][] getDataFromDataprovider(){
         return new Object[][]
                 {
-                     //   { "http://jupiter.cloud.planittesting.com","Stuffed Frog=2,Fluffy Bunny=5,Valentine Bear=3","10.99=2,9.99=5,14.99=3"},
-                        { "http://jupiter2.cloud.planittesting.com","Stuffed Frog=2,Fluffy Bunny=5,Valentine Bear=3","10.99=2,8.99=5,13.99=3"}
+                        { "http://jupiter.cloud.planittesting.com","Stuffed Frog=2,Fluffy Bunny=5,Valentine Bear=3","10.99=2,9.99=5,14.99=3"},
+                     // { "http://jupiter2.cloud.planittesting.com","Stuffed Frog=2,Fluffy Bunny=5,Valentine Bear=3","10.99=2,8.99=5,13.99=3"}
                 };
     }
 
